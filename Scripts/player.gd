d3875fa718139cb19142039e2b6a251e68c54dec
greatbrick0 <speharley@gmail.com>
2023-10-21 15:57:04 -0400

    Added Varying Jump Heights

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,29 +1,42 @@
 extends CharacterBody3D
 
-@export var speed: float = 5.0
-@export var jumpHeight = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@export var speed: Vector2 = Vector2(4, 2)
+@export var smallJumpHeight: float = 4.5
+@export var bigJumpHeight: float = 10
+@export var facingLeft: bool = false
 @export var gravity: float 
+@export var airBrakeMult: float = 4
+
+var input_dir: Vector2
+var holdingJump: bool
+var alreadyJumped: bool
+
+const maxHealth: int = 10
+@export var health: int = 10
+#true is donated, false is equipped [dagger, pendant, shield, boots, sword]
+@export var donatedItems: Array[bool] = [false, false, false, false, false]
+
+func _process(delta):
+	holdingJump = Input.is_action_pressed("Jump")
+	input_dir = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jumpHeight
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
+	if(is_on_floor()):
+		alreadyJumped = false
+		if(holdingJump):
+			alreadyJumped = true
+			velocity.y = smallJumpHeight if donatedItems[3] else bigJumpHeight
+	else:
+		velocity.y -= gravity * delta * (airBrakeMult if velocity.y > 0 and !holdingJump and alreadyJumped else 1)
+	
+	if(input_dir.x != 0):
+		facingLeft = input_dir.x < 0
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.x = direction.x * speed.x
+		velocity.z = direction.z * speed.y
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, speed.x)
+		velocity.z = move_toward(velocity.z, 0, speed.y)
 
 	move_and_slide()
