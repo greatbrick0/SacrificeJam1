60f994a64c6799b7fa9e5044b965878170cdad44
greatbrick <speharley@gmail.com>
2023-10-22 15:14:46 -0400

    Auto stash before merge of "main" and "origin/main"

--- Scripts/rest_area.gd
+++ Scripts/rest_area.gd
@@ -4,9 +4,17 @@ extends Node3D
 @export var dialogue: Array[String] = [""]
 @export var speaking: Array[String] = ["P"]
 
+@export var animsL: Array[String] = [""]
+@export var dialogueL: Array[String] = [""]
+@export var speakingL: Array[String] = ["P"]
+@export var animsR: Array[String] = [""]
+@export var dialogueR: Array[String] = [""]
+@export var speakingR: Array[String] = ["P"]
+
 var tracker: int = 0
 var active: bool = false
 var canContinue: bool = false
+var choicesVisible: bool = false
 
 func _ready():
 	Speak()
@@ -15,13 +23,16 @@ func _ready():
 func _process(delta):
 	if(active):
 		if(Input.is_action_just_pressed("Jump") and canContinue):
-			tracker += 1
-			if(tracker >= len(dialogue)):
-				End()
-			else:
-				canContinue = false
-				$Timer.start()
-				Speak()
+			Progress()
+
+func Progress():
+	tracker += 1
+	if(tracker >= len(dialogue)):
+		End()
+	else:
+		canContinue = false
+		$Timer.start()
+		Speak()
 
 func _on_dialogue_point_area_entered(area):
 	$DialoguePoint/CollisionShape3D.set_deferred("disabled", true)
@@ -39,6 +50,8 @@ func Speak():
 	
 	%YouPanel.get_child(1).text = dialogue[tracker]
 	%StrangerPanel.get_child(1).text = dialogue[tracker]
+	
+	choicesVisible = false
 	if(dialogue[tracker] == ""):
 		ShowChoice()
 	
@@ -53,8 +66,21 @@ func Speak():
 		%StrangerPanel.visible = false
 
 func ShowChoice():
-	pass
+	$Dialogue/Choices.visible = true
+	choicesVisible = true
 
 func End():
 	$Dialogue.visible = false
 	get_tree().get_first_node_in_group("Player").inCutscene = false
+
+func _on_left_button_pressed():
+	anims.append_array(animsL)
+	dialogue.append_array(dialogueL)
+	speaking.append_array(speakingL)
+	Progress()
+
+func _on_right_button_pressed():
+	anims.append_array(animsR)
+	dialogue.append_array(dialogueR)
+	speaking.append_array(speakingR)
+	Progress()
