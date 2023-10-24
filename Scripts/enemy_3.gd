d56b9d95f255776b5fa12df448529f4b6ee0495b
greatbrick <speharley@gmail.com>
2023-10-23 22:02:29 -0400

    Finished Enemy 3

--- Scripts/enemy_3.gd
+++ Scripts/enemy_3.gd
@@ -1,13 +1,14 @@
 extends CharacterBody3D
 
 var playerRef: Player
+var playerDirection: Vector3
 var aggro: bool = false
 @export var aggroRange: float
 @export var speed: float = 1.5
 @export var health: int = 10
 @export var gravity: float = 9
 var fireballObj = preload("res://Scenes/fire_ball.tscn")
-var fireBallRef: Node3D
+var fireballRef: Node3D
 
 func _ready():
 	playerRef = get_tree().get_first_node_in_group("Player")
@@ -45,10 +46,10 @@ func Die():
 
 func _on_attack_cooldown_timeout():
 	if(aggro):
-		fireballObj = fireballObj.instantiate()
-		get_parent().add_child(fireballObj)
-		fireballObj.global_position = global_position
-		fireballObj.playerRef = playerRef
-		var playerDirection: Vector3 = -(global_position - playerRef.global_position).normalized()
-		fireballObj.direction = Vector3(playerDirection.x, playerDirection.y, 0)
+		fireballRef = fireballObj.instantiate()
+		get_parent().add_child(fireballRef)
+		fireballRef.global_position = $SpawnPoint.global_position
+		fireballRef.playerRef = playerRef
+		playerDirection = -($SpawnPoint.global_position - (playerRef.global_position + Vector3(0, -0.6, 0))).normalized()
+		fireballRef.direction = Vector3(playerDirection.x, playerDirection.y, 0)
 		$AttackCooldown.start()
